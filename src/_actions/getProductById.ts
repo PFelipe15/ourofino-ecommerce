7f08b03db181b00e8983b9a6e0e192157aa3761f
PFelipe15 <paulofelipet15@gmail.com>
2024-09-05 18:58:58 -0300

    refactor: refactoring products get

--- src/_actions/getProductById.ts
+++ src/_actions/getProductById.ts
@@ -1,17 +1,17 @@
-
-
-
 "use server";
 
 import type { ProductProps } from "../../types/product-type";
 
 const getProductById = async (id: string | string[] ) => {
 
+  const token = process.env.NEXT_PUBLIC_SHOPIFY_ADMIN_API;
+  if (!token) throw new Error("Token de API Shopify não encontrado");
+
   const product = await fetch(
     `https://6bc81c-70.myshopify.com/admin/api/2024-04/products/${id}.json`,
     {
       headers: {
-        "X-Shopify-Access-Token": process.env.NEXT_PUBLIC_SHOPIFY_ADMIN_API,
+        "X-Shopify-Access-Token": token,
       },
     }
   );
--- src/_actions/getProducts.ts
+++ src/_actions/getProducts.ts
@@ -3,11 +3,15 @@
 import type { ProductProps } from "../../types/product-type";
 
 const getAllProducts = async () => {
+  const token = process.env.NEXT_PUBLIC_SHOPIFY_ADMIN_API;
+  if (!token) throw new Error("Token de API Shopify não encontrado");
+
+
   const products = await fetch(
     `https://6bc81c-70.myshopify.com/admin/api/2024-04/products.json?`,
     {
       headers: {
-        "X-Shopify-Access-Token": process.env.NEXT_PUBLIC_SHOPIFY_ADMIN_API,
+        "X-Shopify-Access-Token": token,
       },
     }
   );
